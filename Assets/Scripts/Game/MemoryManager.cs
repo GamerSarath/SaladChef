8f23d1872bfe70cb35dc4b3de9595e69decc09e3
GamerSarath <sarath886@gmail.com>
2020-02-11 18:43:17 +0530

    The Splash scene is added. The dummy title scene is ready. Events for four major buttons are roughl functioning.Next Settings Scene will be targetted. Rough layout and plan of the entire game is done.

---
+++ Assets/Scripts/Game/MemoryManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MemoryManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        yield return new WaitForSeconds(9.0f);
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
